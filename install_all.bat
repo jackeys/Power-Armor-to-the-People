fdbd0d3095326256f8300acc80bde64d8a66892c
Jack Keys <jackeys@live.ca>
2021-03-13 01:41:34 -0700

    Add a script to link all ESPs to Fallout 4's Data directory

---
+++ install_all.bat
@@ -0,0 +1,12 @@
+@echo off
+
+REM Since this script is intended to be run as administrator, we need to explicitly set our starting directory
+pushd %~dp0
+
+if "%~1" == "" (
+    set /p TARGET_DIRECTORY=Enter absolute path to Fallout 4 install directory: 
+) else (set TARGET_DIRECTORY=%1)
+
+echo Deploying to %TARGET_DIRECTORY%
+
+For /R Content %%F IN (*.esp) do mklink "%TARGET_DIRECTORY%\Data\%%~nxF" "%%F"
