ce50bcc63231685b7469caa112846ba71cc82e9d
Jack Keys <jackeys@live.ca>
2021-03-13 23:33:44 -0600

    Automatically escalate installation script to administrator so it can be run from Windows Explorer

--- install_all.ps1
+++ install_all.ps1
@@ -1,9 +1,25 @@
-$game_directory = $args[0]
+# Self-elevate the script if we aren't already an administrator, since making symbolic links requires Administrator privileges
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CurrentDirectory = Get-Location
+        $GameDirectory = $args[0]
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + "`"$CurrentDirectory`" " + "`"$GameDirectory`""
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList  $CommandLine
+        Exit
+    }
+}
 
-if ($game_directory -eq $null) {
+# This is supplied automatically when the script is run from the mod directory, passed in through the self-elevation above
+$mod_directory = $args[0]
+$game_directory = $args[1]
+
+if ([string]::IsNullOrWhiteSpace($game_directory)) {
     $game_directory = Read-Host -Prompt "Enter the absolute path to your Fallout 4 install directory"
 }
 
+Read-Host -Prompt "Installing all mods to $game_directory -- Press any key to continue"
+
+Set-Location -Path "$mod_directory"
 $files = Get-ChildItem "Content" -Recurse
 
 foreach ($f in $files) {
