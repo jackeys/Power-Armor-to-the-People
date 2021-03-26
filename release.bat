f646e5fdf86a284dbfc3b047c228412dd71931f2
Jack Keys <jackeys@live.ca>
2021-03-26 01:18:29 -0600

    Add a header image to the installer

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 IF "%~1" == "" (set VERSION="%date%") ELSE (set VERSION="%1")
 
-powershell Compress-Archive -Path 'fomod', 'Content' -DestinationPath 'Releases/Power Armor to the People-%VERSION%.zip' -Force
+powershell Compress-Archive -Path 'fomod', 'Content', 'Installer Assets' -DestinationPath 'Releases/Power Armor to the People-%VERSION%.zip' -Force
