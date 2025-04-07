e0a4bce16b5501adf03aaa59426f71c98817ef0e
Jack Keys <jackeys@live.ca>
2025-04-07 12:54:13 -0600

    Add support for 4estGimp's ESL version of TES-51

--- copy_esps_from_game.ps1
+++ copy_esps_from_game.ps1
@@ -38,6 +38,9 @@ foreach ($f in $content_files) {
             } elseif ($mod_file.Contains("Soviet Power Armor\ESL Version")) {
                 $game_file = $game_file -replace '.esp', ' [ESL].esp'
                 Copy-Item -LiteralPath "$game_file" -Destination "$mod_file"
+            } elseif ($mod_file.Contains("TES-51 Power Armor\ESL Version")) {
+                $game_file = $game_file -replace '.esp', ' [ESL].esp'
+                Copy-Item -LiteralPath "$game_file" -Destination "$mod_file"
             } elseif ($mod_file.Contains("Synth Power Armor\ESPFE Version")) {
                 $game_file = $game_file -replace '.esp', ' [ESPFE].esp'
                 Copy-Item -LiteralPath "$game_file" -Destination "$mod_file"
